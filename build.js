2f61ff9641343f5138ecde94f0c4cef84456a445
Nicolas Dufreche <nicolas.dufreche@lp-digital.fr>
2015-04-01 19:27:04 +0200

    update build

--- build.js
+++ build.js
@@ -9,7 +9,7 @@ var fs = require('fs-extra'),
 
     gitPublish = function () {
         var deployUrl = 'https://' + process.env.GIT_TOKEN + '@github.com/backbee/bb-core-js-dist.git';
-        var deployBranch = 'origin';
+        var deployBranch = 'master';
 
         require('child_process').exec('git rev-parse HEAD', function (error, rev) {
             if (process.env.GIT_NAME) {
