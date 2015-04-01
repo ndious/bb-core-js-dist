1eef2a967faa99f47ffb9af4e6be20ad5b1da18b
Nicolas Dufreche <nicolas.dufreche@lp-digital.fr>
2015-04-01 17:21:31 +0200

    testing build

--- build.js
+++ build.js
@@ -1,48 +1,71 @@
 var fs = require('fs-extra'),
+
+    sys = require('sys'),
+
+    exec = require('child_process').execSync,
+
     errorPrint = function (err, message) {
         console.log(message);
         console.error(err);
+    },
+
+    gitPublish = function () {
+        var repo = exec('git config remote.origin.url').replace(/^(?:git@|https:\/\/)/, '').trim(),
+            deployUrl = 'https://' + process.env.GIT_TOKEN + '@' + repo,
+            deployBranch = 'composer',
+            rev = exec('git rev-parse HEAD').trim();
+
+        exec('git config user.name ' + process.env.GIT_NAME);
+        exec('git config user.email ' + process.env.GIT_EMAIL);
+        exec('git add --all');
+        exec('git commit -m "Built from ' + rev + '"');
+        exec('git push -q ' + deployUrl + ' ' + deployBranch);
     };
 
-fs.mkdirs('css', function (err) {
-    if (err) {
+if (parseInt(process.env.TRAVIS_PULL_REQUEST, 10) > 0) {
+    process.exit(0);
+}
+
+console.log('Stating create dist build.');
+
+fs.mkdirs('dist', function (err) {
+     if (err) {
         errorPrint(err, 'CSS dir not created');
     } else {
-        console.log('Strat importing css');
-
-        fs.copy('html/css', 'css/', function (err) {
+        fs.mkdirs('css', function (err) {
             if (err) {
-                errorPrint(err, 'Vendor dir not update from bower_components');
+                errorPrint(err, 'CSS dir not created');
+            } else {
+                console.log('Strat importing css');
+
+                fs.copy('html/css', 'css/', function (err) {
+                    if (err) {
+                        errorPrint(err, 'Vendor dir not update from bower_components');
+                    }
+                });
+            }
+        });
+
+        fs.mkdirs('vendor', function (err) {
+            if (err) {
+                errorPrint(err, 'Vendor dir not created');
+            } else {
+                console.log('Strat importing vendors');
+
+                fs.copy('node_modules/jsclass', 'vendor/jsclass', function (err) {
+                    if (err) {
+                        errorPrint(err, 'Vendor dir not update from node_modules');
+                    }
+                });
+
+                fs.copyRecursive('bower_components/', 'vendor/', function (err) {
+                    if (err) {
+                        errorPrint(err, 'Vendor dir not update from bower_components');
+                    }
+                    gitPublish();
+                });
             }
         });
     }
-});
-
-fs.mkdirs('vendor', function (err) {
-    if (err) {
-        errorPrint(err, 'Vendor dir not created');
-    } else {
-        console.log('Strat importing vendors');
-
-        fs.copy('bower_components/', 'vendor/', function (err) {
-            if (err) {
-                errorPrint(err, 'Vendor dir not update from bower_components');
-            }
-        });
-
-        fs.copy('node_modules/jsclass', 'vendor/jsclass', function (err) {
-            if (err) {
-                errorPrint(err, 'Vendor dir not update from node_modules');
-            }
-        });
-    }
-});
-
-fs.mkdirs('_layouts', function (err) {
-    fs.writeFile('_layouts/base.html.haml', 'last dist', function (err) {
-        if (err) {
-            errorPrint(err, 'Fake haml not created');
-        }
-    });
 })
 
