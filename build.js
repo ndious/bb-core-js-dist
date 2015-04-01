fb5738c22627c2e3a204cf5ecf8fdd0e6de33d9b
undefined <undefined>
2015-04-01 18:52:01 +0200

    update build.js

--- build.js
+++ build.js
@@ -1,7 +1,6 @@
 var fs = require('fs-extra'),
 
-    exec = require('child_process').exec,
-    execSync = require('child_process').execSync,
+    exec = require('child_process').execSync,
 
     errorPrint = function (err, message) {
         console.log(message);
@@ -11,14 +10,15 @@ var fs = require('fs-extra'),
     gitPublish = function () {
         var deployUrl = 'https://' + process.env.GIT_TOKEN + '@github.com/ndufreche/BbCoreJs.git';
         var deployBranch = 'composer';
-        exec('git rev-parse HEAD', function (error, rev) {
+
+        require('child_process').exec('git rev-parse HEAD', function (error, rev) {
             console.log(rev);
 
-            execSync('git config user.name ' + process.env.GIT_NAME);
-            execSync('git config user.email ' + process.env.GIT_EMAIL);
-            execSync('git add --all');
-            execSync('git commit -m "Built from ' + rev + '"');
-            execSync('git push -q ' + deployUrl + ' ' + deployBranch);
+            exec('git config user.name ' + process.env.GIT_NAME);
+            exec('git config user.email ' + process.env.GIT_EMAIL);
+            exec('git add --all');
+            exec('git commit -m "Built from ' + rev + '"');
+            exec('git push -q ' + deployUrl + ' ' + deployBranch);
         });
     };
 
